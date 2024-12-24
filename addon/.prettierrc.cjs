b2d48a0b4e7626b5ebca04afa774ce1da111c8a5
Alexey Kulakov <uni_que@me.com>
2024-12-24 03:11:21 -0800

    feat: v2 migration

---
+++ addon/.prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
+  overrides: [
+    {
+      files: '*.{js,gjs,ts,gts,mjs,mts,cjs,cts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+    {
+      files: '*.{gjs,gts}',
+      options: {
+        singleQuote: true,
+        templateSingleQuote: false,
+      },
+    },
+  ],
+};
